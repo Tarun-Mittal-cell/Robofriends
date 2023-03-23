a10613fc4d04e66e55cf86421a32eb78b5d32b26
Tarun-Mittal-cell <tm6622@g.rit.edu>
2023-03-23 04:27:10 -0400

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React , { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+
+
+class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=> this.setState({robots: users}));
+    }
+
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robots => {
+            return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+
+        if(!robots.length){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+        
+            );
+        }
+    }
+   
+}
+
+export default App;
